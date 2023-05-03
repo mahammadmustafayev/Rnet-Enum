c7dd324d07e1f2e89ad6c00dd1318c2ad0204522
mmustafayev213-3 <mustafamehmed1251@gmail.com>
2023-05-03 16:45:41 +0400

    added enum

---
+++ EnumDisplay/EnumDisplay/Models/Customer.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnumDisplay.Models
+{
+    public enum Status:byte
+    {
+        None=0,
+        [Display(Name ="Aktif Kullanicilar Listeler")]
+        Active=1,
+        [Display(Name = "Passive Kullanicilar Listeler")]
+        Passive =2,
+        [Display(Name = "Deleted Kullanicilar Listeler")]
+        Deleted =3,
+        [Display(Name = "Holiday Kullanicilar Listeler")]
+        Holiday
+    }
+    public class Customer
+    {
+        public Status Status { get; set; }
+        public void GetDisplayName()
+        {
+            var properties=typeof(Status).GetFields();
+            foreach (var pp in properties)
+            {
+                var da=pp.GetCustomAttribute(typeof(DisplayAttribute)) as DisplayAttribute ;
+                if (da!=null)
+                {
+                    Console.WriteLine(pp.Name+" -> "+da.Name);
+                }
+            }
+        }
+    }
+}
---
+++ EnumDisplay/EnumDisplay/Program.cs
@@ -0,0 +1,13 @@
+﻿using EnumDisplay.Models;
+
+namespace EnumDisplay
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer customer = new();
+            customer.GetDisplayName();
+        }
+    }
+}
